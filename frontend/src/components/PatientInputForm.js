9b5ecc3672ca860ff2245daee29c67e82feab3c9
batu <batu@192.168.1.34>
2025-07-18 20:34:15 -0300

    validation layer added to the input form

--- frontend/src/components/PatientInputForm.js
+++ frontend/src/components/PatientInputForm.js
@@ -1,6 +1,8 @@
 import InputText from 'primevue/inputtext'
 import Button from 'primevue/button'
 import Dialog from 'primevue/dialog';
+import Message from 'primevue/message';
+
 
 
 export default {
@@ -18,15 +20,74 @@ export default {
             filename:"",
             filetype:""
         },
-        msg_status: ""
+        msg_status: "",
+        invalid_email: false,
+        invalid_name: false,
+        invalid_lastname: false,
+        invalid_file: false,
+        val_errors: {
+            email: "",
+            name: "",
+            lastname: "",
+            file: ""
+        }
     };
   },
   components:{
     InputText,
     Button,
-    Dialog
+    Dialog,
+    Message
   },
   methods: {
+
+    resetValidation(){
+        // Reset all errors before validation
+        this.invalid_email = false;
+        this.invalid_name = false;
+        this.invalid_lastname = false;
+        this.invalid_file = false;
+
+        this.val_errors = {
+        email: "",
+        name: "",
+        lastname: "",
+        file: ""
+        };
+    },
+
+    //validation layer
+    validateForm(){
+        this.resetValidation();
+
+        let valid = true;
+
+        if(!this.form.email.endsWith('@gmail.com')){
+            this.val_errors.email = "Invalid email, only @gmail.com is accepted"
+            this.invalid_email = true
+            valid = false
+        }
+        if(!/^[A-Za-z\s]*$/.test(this.form.name)){
+           this.val_errors.name = "Name must contain only letters."
+            this.invalid_name = true
+            valid = false
+
+        }
+
+        if(!/^[A-Za-z\s]*$/.test(this.form.lastname)){
+           this.val_errors.lastname = "Lastname must contain only letters."
+            this.invalid_lastname = true
+            valid = false
+        }
+
+        if(!this.form.filename.endsWith(".jpg")){
+            this.val_errors.file = "Document photo .jpg is required."
+            this.invalid_file = true
+            valid = false
+        }
+        return valid
+    },
+
     handleFileChange(event) {
         const selectedFile = event.target.files[0];
         if (selectedFile) {
@@ -48,6 +109,11 @@ export default {
       if (!name || !lastname ||!email || !phone || !code || !file) {
         this.visible = true
         this.msg_status = 'Please complete all fields'
+        return;
+      }
+      if(!this.validateForm()){
+        this.visible = true;
+        this.msg_status = 'Some fields are invalid. Please correct them to continue.';
         return;
       }
 
@@ -86,16 +152,19 @@ export default {
 
         <div>
             <InputText id="name" v-model="form.name" class="form-input" placeholder="John" variant="filled" />
+            <Message v-if="invalid_name" severity="error" size="small" variant="simple">{{ val_errors.name  }}</Message>
             <label for="name">Name</label>
         </div>
 
         <div>
             <InputText id="lastname" v-model="form.lastname" class="form-input" placeholder="Doe" variant="filled" />
+            <Message v-if="invalid_lastname" severity="error" size="small" variant="simple">{{ val_errors.lastname  }}</Message>
             <label for="lastname">Lastname</label>
         </div>
 
         <div>
-            <InputText id="email" v-model="form.email" class="form-input" placeholder="john@example.com" variant="filled"/>
+            <InputText id="email" v-model="form.email" class="form-input" placeholder="john@gmail.com" variant="filled"/>
+            <Message v-if="invalid_email" severity="error" size="small" variant="simple">{{ val_errors.email}}</Message>
             <label for="email">Email</label>
         </div>
 
@@ -113,6 +182,7 @@ export default {
         <div class="file-upload">
             <label for="file">Upload Document</label>
             <input type="file" id="file" @change="handleFileChange" class="custom-file-input" />
+            <Message v-if="invalid_file" severity="error" size="small" variant="simple">{{ val_errors.file}}</Message>
             <p v-if="form.filename" class="file-name"> {{ form.filename }}</p>
         </div>
 
